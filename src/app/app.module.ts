6a22d62303d0c85ad31b1a79daaf2c81c8b13e26
Morten <mrtnsund@gmail.com>
2020-03-31 18:19:45 +0200

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    EventService,
+    ToastrService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
