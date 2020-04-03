01a3e539a52cd776f0462eaa437b2281d4b14166
Morten <mrtnsund@gmail.com>
2020-04-03 16:00:13 +0200

    la til strong typing

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared';
 
 @Component({
   selector: 'event-thumbnail',
@@ -33,7 +34,7 @@ import { Component, Input } from '@angular/core';
   `]
 })
 export class EventThumbnailComponent {
-  @Input() event: any;
+  @Input() event: IEvent;
 
   // getStartTimeClass(): string {
   //   // const isEarlyStart = this.event && this.event.time === '8:00 am';
@@ -44,7 +45,7 @@ export class EventThumbnailComponent {
   //   return '';
   // }
 
-  getStartTimeStyle() {
+  getStartTimeStyle(): any {
     if (this.event && this.event.time === '8:00 am') {
       return {color: '#003300', 'font-weight': 'bold'};
     }
